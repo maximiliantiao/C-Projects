2d2c83dfebbf9d1ae20b4cb09aa5a394d115e7df
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-06-18 15:26:24 -0700

    Implemented 5 more mathematical operations

--- calculator.c
+++ calculator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+// Functions that perform mathematical operations 
 
 double multiplication(double num1, double num2) {
 	double resultMult = num1 * num2;
@@ -22,26 +25,30 @@ double subtraction(double num1, double num2) {
 }
 
 double average(double num1, double num2) {
-	double resultAvg = (num1 + num2) / 2;
+	double resultAvg = (num1 + num2) / 2.0;
 	return resultAvg;
 }
 
 double absolute(double num1) {
-	double resultAbs = abs(num1);
-	return resultAbs;
-}
-
-double cToF(double num1) {
-	double resultCToF = ((9/5) * num1) + 32;
-	return resultCToFl
+	if (num1 >= 0.0) {
+		double resultAbs = num1;
+		return resultAbs;
+	} else {
+		double resultAbs = -(num1);
+		return resultAbs;
+	}
 }
 
 double fToC(double num1) {
-	double resultFToC = (32 - num1) * (5/9);
+	double resultFToC = (((num1 - 32) * 5) / 9);
 	return resultFToC;
 }
 
-double tanget(int)
+double cToF(double num1) {
+	double resultCToF = ((9 * num1) / 5) + 32.0;
+	return resultCToF;
+}
+
 
 int main() {
 
@@ -58,47 +65,103 @@ int main() {
 	printf("Subtraction: -\n");
 	printf("Average: v\n");
 	printf("Absolute value: a\n");
-	printf("Celsius to Fahrenheit: c\n");
-	printf("Fahrenheit to Celsius: f\n");
+	printf("Fahrenheit to Celsius: c\n");
+	printf("Celsius to Fahrenheit: f\n");
 	printf("Tanget: t\n\n");
 
 	printf("Enter an operation: ");
 	scanf(" %c", &operation);
 
-	/* Prompt user for 2 numbers*/
-	double num1;
-	printf("Enter the first number: ");
-	scanf("%d", &num1);
-
-	double num2;
-	printf("Enter the second number: ");
-	scanf("%d", &num2);
-
-	/* Display result header */
-	printf("\nResult\n\n");
-
 	/* Based on user's operation, call functions for the operation */
-	if (operation == '+') {
+	if (operation == '*') {
 
-		printf("%d + %d = %d\n", num1, num2, addition(num1, num2));
+		double num1;
+		printf("Enter the first number: ");
+		scanf("%lf", &num1);
 
-	} else if (operation == '-') {
+		double num2;
+		printf("Enter the second number: ");
+		scanf("%lf", &num2);
 
-		printf("%d - %d = %d\n", num1, num2, subtraction(num1, num2));
-
-	} else if (operation == '*') {
-
-		printf("%d * %d = %d\n", num1, num2, multiplication(num1, num2));
+		printf("Result of (%lf * %lf) : %lf\n", num1, num2, multiplication(num1, num2));
 
 	} else if (operation == '/') {
 
-		printf("%d / %d = %d\n", num1, num2, division(num1, num2));
+		double num1;
+		printf("Enter the first number: ");
+		scanf("%lf", &num1);
+
+		double num2;
+		printf("Enter the second number: ");
+		scanf("%lf", &num2);
+
+		printf("Result of (%lf / %lf) : %lf\n", num1, num2, division(num1, num2));
+
+	} else if (operation == '+') {
+
+		double num1;
+		printf("Enter the first number: ");
+		scanf("%lf", &num1);
+
+		double num2;
+		printf("Enter the second number: ");
+		scanf("%lf", &num2);
+
+		printf("Result of (%lf + %lf) : %lf\n", num1, num2, addition(num1, num2));
+
+	} else if (operation == '-') {
+
+		double num1;
+		printf("Enter the first number: ");
+		scanf("%lf", &num1);
+
+		double num2;
+		printf("Enter the second number: ");
+		scanf("%lf", &num2);
+
+		printf("Result of (%lf - %lf) : %lf\n", num1, num2, subtraction(num1, num2));
+
+	} else if (operation == 'v') {
+
+		double num1;
+		printf("Enter the first number: ");
+		scanf("%lf", &num1);
+
+		double num2;
+		printf("Enter the second number: ");
+		scanf("%lf", &num2);
+
+		printf("Result of (%lf v %lf) : %lf\n", num1, num2, average(num1, num2));
+
+	} else if (operation == 'a') {
+
+		double num1;
+		printf("Enter a number: ");
+		scanf("%lf", &num1);
+
+		printf("Result of |%lf| : %lf\n", num1, absolute(num1));
+
+	} else if (operation == 'c') {
+
+		double num1;
+		printf("Enter a number: ");
+		scanf("%lf", &num1);
+
+		printf("Result of (%lf deg -> C) : %lf\n", num1, fToC(num1));
+
+	} else if (operation == 'f') {
+
+		double num1;
+		printf("Enter a number: ");
+		scanf("%lf", &num1);
+
+		printf("Result of (%lf deg -> C) : %lf\n", num1, cToF(num1));
 
 	} else {
 
-		printf("Invalid operation\n");
-
+		printf("Invalid operator");
 	}
+
 
 	return 0;
 }
