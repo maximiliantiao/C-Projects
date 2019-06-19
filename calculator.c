b9aaac5f99f9eb82cbe03832422b293ea123df7f
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-06-19 10:41:07 -0700

    Implemeneted suggested outline and layout of code

--- calculator.c
+++ calculator.c
@@ -49,119 +49,95 @@ double cToF(double num1) {
 	return resultCToF;
 }
 
-
+double tangetOp(double num1) {
+	double degToRad = ((3.14159 * num1) / 180);
+	double resultTan = tan(degToRad);
+	return resultTan; 
+}
 int main() {
 
 	/* Prompt user with nice greeting */
 	printf("\nHello there!\nWelcome to Calculator.exe\n\n");
 
 	/* Prompt user for mathematical operation for the two numbers */
-	char operation;
+	while (1) {
+		char operation;
 
-	printf("List of operations\n\n");
-	printf("Multiplication: *\n");
-	printf("Division: /\n");
-	printf("Addition: +\n");
-	printf("Subtraction: -\n");
-	printf("Average: v\n");
-	printf("Absolute value: a\n");
-	printf("Fahrenheit to Celsius: c\n");
-	printf("Celsius to Fahrenheit: f\n");
-	printf("Tanget: t\n\n");
+		printf("List of operations\n\n");
+		printf("Multiplication: *\n");
+		printf("Division: /\n");
+		printf("Addition: +\n");
+		printf("Subtraction: -\n");
+		printf("Average: v\n");
+		printf("Absolute value: a\n");
+		printf("Fahrenheit to Celsius: c\n");
+		printf("Celsius to Fahrenheit: f\n");
+		printf("Tanget: t\n\n");
 
-	printf("Enter an operation: ");
-	scanf(" %c", &operation);
+		printf("Enter an operation: ");
+		scanf(" %c", &operation);
 
-	/* Based on user's operation, call functions for the operation */
-	if (operation == '*') {
+		if (operation == '*' || operation == '/' || operation == '+' || operation == '-' || operation == 'v') {
 
-		double num1;
-		printf("Enter the first number: ");
-		scanf("%lf", &num1);
+			double num1;
+			printf("Enter the first number: ");
+			scanf("%lf", &num1);
 
-		double num2;
-		printf("Enter the second number: ");
-		scanf("%lf", &num2);
+			double num2;
+			printf("Enter the second number: ");
+			scanf("%lf", &num2);
 
-		printf("Result of (%lf * %lf) : %lf\n", num1, num2, multiplication(num1, num2));
+			if (operation == '*') {
 
-	} else if (operation == '/') {
+				printf("Result of (%lf * %lf) : %lf\n\n", num1, num2, multiplication(num1, num2));
 
-		double num1;
-		printf("Enter the first number: ");
-		scanf("%lf", &num1);
+			} else if (operation == '/') {
 
-		double num2;
-		printf("Enter the second number: ");
-		scanf("%lf", &num2);
+				printf("Result of (%lf / %lf) : %lf\n\n", num1, num2, division(num1, num2));
 
-		printf("Result of (%lf / %lf) : %lf\n", num1, num2, division(num1, num2));
+			} else if (operation == '+') {
 
-	} else if (operation == '+') {
+				printf("Result of (%lf + %lf) : %lf\n\n", num1, num2, addition(num1, num2));
 
-		double num1;
-		printf("Enter the first number: ");
-		scanf("%lf", &num1);
+			} else if (operation == '-') {
 
-		double num2;
-		printf("Enter the second number: ");
-		scanf("%lf", &num2);
+				printf("Result of (%lf - %lf) : %lf\n\n", num1, num2, subtraction(num1, num2));
 
-		printf("Result of (%lf + %lf) : %lf\n", num1, num2, addition(num1, num2));
+			} else {
 
-	} else if (operation == '-') {
+				printf("Result of (%lf v %lf) : %lf\n\n", num1, num2, average(num1, num2));
 
-		double num1;
-		printf("Enter the first number: ");
-		scanf("%lf", &num1);
+			}
 
-		double num2;
-		printf("Enter the second number: ");
-		scanf("%lf", &num2);
+		} else if (operation == 'a' || operation == 'c' || operation == 'f' || operation == 't') {
 
-		printf("Result of (%lf - %lf) : %lf\n", num1, num2, subtraction(num1, num2));
+			double num1;
+			printf("Enter a number: ");
+			scanf("%lf", &num1);
 
-	} else if (operation == 'v') {
+			if (operation == 'a') {
 
-		double num1;
-		printf("Enter the first number: ");
-		scanf("%lf", &num1);
+				printf("Result of |%lf| : %lf\n\n", num1, absolute(num1));
 
-		double num2;
-		printf("Enter the second number: ");
-		scanf("%lf", &num2);
+			} else if (operation == 'c') {
 
-		printf("Result of (%lf v %lf) : %lf\n", num1, num2, average(num1, num2));
+				printf("Result of (%lf deg -> C) : %lf\n\n", num1, fToC(num1));
 
-	} else if (operation == 'a') {
+			} else if (operation == 'f') {
 
-		double num1;
-		printf("Enter a number: ");
-		scanf("%lf", &num1);
+				printf("Result of (%lf deg -> C) : %lf\n\n", num1, cToF(num1));
 
-		printf("Result of |%lf| : %lf\n", num1, absolute(num1));
+			} else if (operation == 't') {
 
-	} else if (operation == 'c') {
+				printf("Result of tan(%lf): %lf\n\n", num1, tangetOp(num1));
 
-		double num1;
-		printf("Enter a number: ");
-		scanf("%lf", &num1);
+			} 
 
-		printf("Result of (%lf deg -> C) : %lf\n", num1, fToC(num1));
+		} else {
 
-	} else if (operation == 'f') {
+			printf("Invalid operation!\n\n");
 
-		double num1;
-		printf("Enter a number: ");
-		scanf("%lf", &num1);
-
-		printf("Result of (%lf deg -> C) : %lf\n", num1, cToF(num1));
-
-	} else {
-
-		printf("Invalid operator");
-	}
-
-
+		}
+	}	
 	return 0;
 }
