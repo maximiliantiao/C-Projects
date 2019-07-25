da7369c0faff3475b1a93b3b15cbd457dcf2ee8c
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-07-24 21:06:16 -0700

    Implemented function for Player vs Player

--- guessTheNumber.c
+++ guessTheNumber.c
@@ -1,8 +1,9 @@
-/* Guess the Number game
+// Guess the Number game
 
 
-3 gamemodes
+// 3 gamemodes
 
+/*
 1. Player vs. Player (PvP)
 
 	Player 1 will type in a number between 1 and 100, Player 2 will guess Player 1's number
@@ -16,8 +17,6 @@
 
 	Computer 1 will randomly select a number, Computer 2 will guess the Computer 1's number
 
-
-
 Gamemode 1 -- PvP
 
 	scanf for Player 1 number
@@ -26,13 +25,13 @@ Gamemode 1 -- PvP
 
 Gamemode 2 -- PvC
 
-	Computer uses random number generator between 1 and 100 and select the random number
+	Computer uses random number generator between 1 and 50 and select the random number
 	prompt Player for guesses (10 guesses)
 
 Gamemode 3 -- CvC
 
-	Computer 1 uses random number generator between 1 and 100 and select the random number
-	Computer 2 also uses a random generator between 1 and 100 and guesses the right number
+	Computer 1 uses random number generator between 1 and 50 and select the random number
+	Computer 2 also uses a random generator between 1 and 50 and guesses the right number
 	For this gamemode, Computer 2 will have unlimited guesses, Player can watch how many guesses
 	it took for Computer 2 to guess Computer 1's number
 
@@ -48,3 +47,93 @@ Option to play game again -> use while loop or something
 
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+void P2P() {
+
+	int Player1_Number;
+	printf("Player 1 type in a number between 1 and 50: ");
+	scanf("%d", &Player1_Number);
+	printf("\n\n");
+
+	printf("Player 2 has 10 guesses to get the right number.\n\n");
+	printf("Begin guessing...");
+
+	int Player2_GuessesLeft = 10;	
+	while (Player2_GuessesLeft == 0) {
+
+		int Player2_Guess;
+		printf("Guess: "); 
+		scanf("%d", &Player2_Guess);
+		print("\n\n");
+
+		if (Player2_Guess == Player1_Number) {
+			printf("Correct Guess")
+			Player2_GuessesLeft = 0;
+		} else {
+			printf("Incorrect Guess\n");
+			printf("Try Again");
+			Player2_GuessesLeft--;
+		}
+
+	}
+
+
+
+
+
+
+}
+
+int main() {
+
+	printf("Welcome to Guess the Number!\n\n");
+
+	printf("3 Gamemodes\n\n");
+
+	printf("1) Player vs. Player\n");
+	printf("Player 1 will type in a number between 1 and 50,\nPlayer 2 will guess Player 1's number
+	Player 2 has 10 guesses\n\n");
+
+	printf("2) Player vs. Computer\n\n");
+	printf("Computer will randomly select a number,\nPlayer will guess the Computer's number\n\n");
+
+	printf("3) Computer vs. Computer\n\n");
+	printf("Computer 1 will randomly select a number,\nComputer 2 will guess the Computer 1's number\n\n");
+
+	int gamemode_choice;
+	printf("Select a gamemode and type the number --> ");
+	scanf("%d", &gamemode_choice);
+
+	if (gamemode_choice == 1) {
+		P2P();
+	} else if (gamemode_choice == 2) {
+		P2C();
+	} else if (gamemode_choice == 3) {
+		C2C();
+	} else {
+		printf("Please enter a valid gamemode!");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
