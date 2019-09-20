0766f5485a6e8a15479a39dd887f0034d0c97593
Maximilian Tiao <maximilian.tiao@gmail.com>
2019-09-19 21:22:29 -0700

    Implemented functions that will assist in finding matches within the game

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -2,12 +2,160 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int matchesTopRow(char ticTacToe[3][3]) {
+	// Check for any matches on the top row
+	int e;
+	int numberOfXsInTopRow = 0;
+	int numberOfOsInTopRow = 0;
+	for (e = 0; e < 3; e++) {
+	  if (ticTacToe[0][e] == 'O') {
+	    numberOfOsInTopRow++;
+	  }
+		if (ticTacToe[0][e] == 'X') {
+	    numberOfXsInTopRow++;
+	  }
+	}
+
+	if (numberOfOsInTopRow == 3) {
+	  return 1;
+	} else if (numberOfXsInTopRow == 3) {
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesMidRow(char ticTacToe[3][3]) {
+	// Check for any matches on the top row
+	int f;
+	int numberOfXsInMiddleRow = 0;
+	int numberOfOsInMiddleRow = 0;
+	for (f = 0; f < 3; f++) {
+	  if (ticTacToe[1][f] == 'O') {
+	    numberOfOsInMiddleRow++;
+	  }
+		if (ticTacToe[1][f] == 'X') {
+	    numberOfXsInMiddleRow++;
+	  }
+	}
+
+	if (numberOfOsInMiddleRow == 3) {
+	  return 1;
+	} else if (numberOfXsInMiddleRow == 3) {
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesBottomRow(char ticTacToe[3][3]) {
+	// Checks for any matches on the bottom row
+	int g;
+	int numberOfXsInBottomRow = 0;
+	int numberOfOsInBottomRow = 0;
+	for (g = 0; g < 3; g++) {
+	  if (ticTacToe[2][g] == 'O') {
+	    numberOfOsInBottomRow++;
+	  }
+	 	if (ticTacToe[2][g] == 'X') {
+	    numberOfXsInBottomRow++;
+	  }
+	}
+
+	if (numberOfOsInBottomRow == 3) {
+	  return 1;
+	} else if (numberOfXsInBottomRow == 3) {
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesFirstCol(char ticTacToe[3][3]) {
+	// Checks for any matches on the first column
+	int h;
+	int numberOfXsInFirstColumn = 0;
+	int numberOfOsInFirstColumn = 0;
+	for (h = 0; h < 3; h++) {
+	  if (ticTacToe[h][0] == 'O') {
+	    numberOfOsInFirstColumn++;
+	  }
+		if (ticTacToe[h][0] == 'X') {
+	    numberOfXsInFirstColumn++;
+	  }
+	}
+
+	if (numberOfOsInFirstColumn == 3) {
+	  return 1;
+	} else if (numberOfXsInFirstColumn == 3) {
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesSecondCol(char ticTacToe[3][3]) {
+	// Checks for any matches on the second column
+	int i;
+	int numberOfOsInSecondColumn = 0;
+	int numberOfXsInSecondColumn = 0;
+	for (i = 0; i < 3; i++) {
+	  if (ticTacToe[i][1] == 'O') {
+	    numberOfOsInSecondColumn++;
+	  }
+		if (ticTacToe[i][1] == 'X') {
+	    numberOfXsInSecondColumn++;
+	  }
+	}
+
+	if (numberOfOsInSecondColumn == 3) {
+	  return 1;
+	} else if (numberOfXsInSecondColumn == 3){
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesThirdCol(char ticTacToe[3][3]) {
+	// Checks for any matches on the third column
+	int j;
+	int numberOfXsInThirdColumn = 0;
+	int numberOfOsInThirdColumn = 0;
+	for (j = 0; j < 3; j++) {
+	  if (ticTacToe[j][2] == 'O') {
+	    numberOfOsInThirdColumn++;
+	  }
+		if (ticTacToe[j][2] == 'X')  {
+	    numberOfXsInThirdColumn++;
+	  }
+	}
+
+	if (numberOfOsInThirdColumn == 3) {
+	  return 1;
+	} else if (numberOfXsInThirdColumn == 3) {
+	  return 2;
+	} else {
+	  return 0;
+	}
+}
+
+int matchesDiagonalA(char ticTacToe[3][3]) {
+	// Check for any matches on diagonal from top left to botton right
+}
+
+int matchesDiagonalB(char ticTacToe[3][3]) {
+	// Check for any matches on diagonal from bottom left to top right
+}
+
+
 int main() {
 
-
+	// 3 x 3 Grid Setup
 	char ticTacToe[3][3] = {{'*', '*', '*'}, {'*', '*', '*'}, {'*', '*', '*'}};
 	char space = ' ';
 
+	// Welcome screen + Menu options
 	printf("Welcome to Tic-Tac-Toe!\n\n");
 
 	int gameMode;
@@ -19,15 +167,15 @@ int main() {
 
 	scanf("\n\n%d", &gameMode);
 
-	if (gameMode == 1) {
-
+	// Gamemode 1
+	if (gameMode == 1) {/*
 		int row, column;
 		int counter = 0;
 
+		// Display 3 x 3 Grid
 		while(counter <= 9) {
 
 			printf("-----------\n");
-
 			int a, b;
 			for (a = 0; a < 3; a++) {
 				printf("|");
@@ -40,10 +188,9 @@ int main() {
 				printf("\n");
 			}
 
+			// Player 1's Turn
 			printf("-----------\n\n");
-
 			printf("Player 1's Turn\n\n");
-
 			printf("Enter a row number (0, 1, 2): ");
 			scanf("%d", &row);
 
@@ -52,56 +199,45 @@ int main() {
 			printf("\n\n");
 
 			if (ticTacToe[row][column] == '*') {
-
-				ticTacToe[row][column] = '0';
+				ticTacToe[row][column] = 'O';
 
 			} else if (ticTacToe[row][column] == 'X') {
-
 				printf("This has already been chosen!\n\n");
-
 				while (ticTacToe[row][column] == 'X') {
-
-					if (ticTacToe[row][column] == 'X') { 
+					if (ticTacToe[row][column] == 'X') {
 						printf("Enter a row number (0, 1, 2): ");
 						scanf("%d", &row);
 
 						printf("Enter a column number (0, 1, 2): ");
 						scanf("%d", &column);
 						printf("\n\n");
-
 					} else {
 						break;
 					}
 				}
 
-			
+
 			} else if (ticTacToe[row][column] == 'O') {
 
 				printf("This has already been chosen!\n\n");
-
 				while (ticTacToe[row][column] == 'O') {
-
-					if (ticTacToe[row][column] == '0') { 
+					if (ticTacToe[row][column] == 'O') {
 						printf("Enter a row number (0, 1, 2): ");
 						scanf("%d", &row);
 
 						printf("Enter a column number (0, 1, 2): ");
 						scanf("%d", &column);
 						printf("\n\n");
-
 					} else {
 						break;
 					}
 				}
-
 			} else {
-
 				printf("Invalid row and/or column number!");
-				
 			}
 
+			// Player 2's Turn
 			printf("-----------\n");
-
 			int c, d;
 			for (c = 0; c < 3; c++) {
 				printf("|");
@@ -115,9 +251,7 @@ int main() {
 			}
 
 			printf("-----------\n\n");
-
 			printf("Player 2's Turn\n\n");
-
 			printf("Enter a row number (0, 1, 2): ");
 			scanf("%d", &row);
 
@@ -127,179 +261,50 @@ int main() {
 
 
 			if (ticTacToe[row][column] == '*') {
-
 				ticTacToe[row][column] = 'X';
-
 			} else if (ticTacToe[row][column] == 'X') {
-
 				printf("This has already been chosen!");
-
 				while (ticTacToe[row][column] == 'X') {
-
-					if (ticTacToe[row][column] == 'X') { 
+					if (ticTacToe[row][column] == 'X') {
 						printf("Enter a row number (0, 1, 2): ");
 						scanf("%d", &row);
 
 						printf("Enter a column number (0, 1, 2): ");
 						scanf("%d", &column);
 						printf("\n\n");
-
 					} else {
 						break;
 					}
 				}
-			
+
 			} else if (ticTacToe[row][column] == 'O') {
-
 				printf("This has already been chosen!");
-
 				while (ticTacToe[row][column] == 'O') {
-
-					if (ticTacToe[row][column] == 'O') { 
+					if (ticTacToe[row][column] == 'O') {
 						printf("Enter a row number (0, 1, 2): ");
 						scanf("%d", &row);
 
 						printf("Enter a column number (0, 1, 2): ");
 						scanf("%d", &column);
 						printf("\n\n");
-
 					} else {
 						break;
 					}
 				}
-
 			} else {
-
 				printf("Invalid row and/or column number!\n\n");
-				
 			}
-
 			counter++;
-
 		}
+	*/
+		//int matchTop = matchesTopRow(ticTacToe);
+		//int matchMid = matchesMidRow(ticTacToe);
+		//int matchBottom = matchesBottomRow(ticTacToe);
+		//int matchFirstCol = matchesFirstCol(ticTacToe);
+		//int matchSecondCol = matchesSecondCol(ticTacToe);
+		//int matchThirdCol = matchesThirdCol(ticTacToe);
 
-		int e;
-		int numberOfXsInTopRow;
-		int numberOfOsInTopRow;
-		for (e = 0; e < 3; e++) {
-			if (ticTacToe[0][e] == 'O') {
-				numberOfOsInTopRow++;
-			} else {
-				numberOfXsInTopRow++;
-			}
-		}
-
-		if (numberOfOsInTopRow == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInTopRow == 3) {
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in the top row!");
-		}
-
-		int f;
-		int numberOfXsInMiddleRow;
-		int numberOfOsInMiddleRow;
-		for (f = 0; f < 3; f++) {
-			if (ticTacToe[1][f] == 'O') {
-				numberOfOsInMiddleRow++;
-			} else {
-				numberOfXsInMiddleRow++;
-			}
-		}
-
-		if (numberOfOsInMiddleRow == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInMiddleRow == 3) {
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in the middle row!");
-		}
-
-		int g;
-		int numberOfXsInBottomRow;
-		int numberOfOsInBottomRow;
-		for (g = 0; g < 3; g++) {
-			if (ticTacToe[2][g] == 'O') {
-				numberOfOsInBottomRow++;
-			} else {
-				numberOfXsInBottomRow++;
-			}
-		}
-
-		if (numberOfOsInBottomRow == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInBottomRow == 3) {
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in the bottom row!");
-		}
-
-		int h;
-		int numberOfXsInFirstColumn;
-		int numberOfOsInFirstColumn;
-		for (h = 0; h < 3; h++) {
-			if (ticTacToe[h][0]) {
-				numberOfOsInFirstColumn++;
-			} else {
-				numberOfXsInFirstColumn++;
-			}
-		}
-
-		if (numberOfOsInFirstColumn == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInFirstColumn == 3) {
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in the first column!");
-		}
-
-		int i;
-		int numberOfOsInSecondColumn;
-		int numberOfXsInSecondColumn;
-		for (i = 0; i < 3; i++) {
-			if (ticTacToe[i][1]) {
-				numberOfOsInSecondColumn++;
-			} else {
-				numberOfXsInSecondColumn++;
-			}
-		}
-
-		if (numberOfOsInSecondColumn == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInSecondColumn == 3){
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in the second column!");
-		}
-
-		int j;
-		int numberOfXsInThirdColumn;
-		int numberOfOsInThirdColumn;
-		for (j = 0; j < 3; j++) {
-			if (ticTacToe[j][2]) {
-				numberOfOsInThirdColumn++;
-			} else {
-				numberOfXsInThirdColumn++;
-			}
-		}
-
-		if (numberOfOsInThirdColumn == 3) {
-			printf("Player 1 wins!");
-		} else if (numberOfXsInThirdColumn == 3) {
-			printf("Player 2 wins!");
-		} else {
-			printf("No matches in third column!");
-		}
-
-	} else if (gameMode == 2) {
-
-		printf("Coming Soon");
-
-	} else if (gameMode == 3) {
-
-		printf("Coming Soon");
-
+ 		printf("%d", matchThirdCol);
 	} else {
 		printf("Invalid game mode");
 	}
