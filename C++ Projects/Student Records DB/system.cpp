212a93a5e69a2cbb6366143ba9f1a9b58221aac8
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-03-20 21:31:56 -0700

    Made some changes

--- C++ Projects/Student Records DB/system.cpp
+++ C++ Projects/Student Records DB/system.cpp
@@ -4,9 +4,6 @@
 #include <sqlite3.h>
 #include <string>
 
-/*
-TODO:
-*/
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
   for(int i = 0; i<argc; i++) {
     printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
