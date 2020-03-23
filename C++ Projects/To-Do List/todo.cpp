7605b9dca5d157ab4f8e6f60f863c8558c318c70
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-03-23 10:20:56 -0700

    New program idea

---
+++ C++ Projects/To-Do List/todo.cpp
@@ -0,0 +1,28 @@
+/*
+
+TO_DO List Program
+
+struct to_do_item {
+  std::string item;
+  int priority;
+  std:string date;
+}
+
+array_of_structs = [to_do_item, to_do_item...];
+
+print out to_do list in the following ways:
+1. By Priority Number
+2. Alphabetical based on item
+3. No Order
+4. Simple View / Detailed View
+
+Features:
+1. Add item
+2. Delete item
+3. Update item
+
+
+Future features:
+1. Create several to_do lists
+2. More member definitions in to_do_item struct
+*/
