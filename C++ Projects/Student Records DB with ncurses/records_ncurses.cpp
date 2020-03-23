2715e961bb4aa95e8a173a6fe9eba40c513157bb
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-03-22 19:50:51 -0700

    Created separate functions for add, update, and delete student entry, also added color to main menu

--- C++ Projects/Student Records DB with ncurses/records_ncurses.cpp
+++ C++ Projects/Student Records DB with ncurses/records_ncurses.cpp
@@ -3,7 +3,21 @@
 #include <fstream>
 #include <sqlite3.h>
 #include <string.h>
+#include <unistd.h>
 #include <ncurses.h>
+
+std::string str_name;
+std::string str_phone_number;
+std::string str_email;
+std::string str_dept;
+std::string str_title;
+std::string str_aff;
+char name[100];
+char phone_number[100];
+char email[100];
+char dept[100];
+char title[100];
+char aff[100];
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
   if (argc == 0) {
@@ -19,11 +33,187 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
   return 0;
 }
 
-void setZero(char *info) {
-  for (int i = 0; i < sizeof(info); i++) {
-    info[i] = 0;
+void add_entry(std::string select, std::string insert, sqlite3 *db, char *error_msg, int rc) {
+  printw("\n\n***** Adding student entry *****\n\n");
+  refresh();
+
+  select = "SELECT * FROM BANKSYS;";
+  rc = sqlite3_exec(db, select.c_str(), NULL, 0, &error_msg);
+  if (rc != SQLITE_OK) {
+    printw("Error: Cannot execute SELECT Query in ADD ENTRY\n");
+    refresh();
+    exit(-1);
   }
-  return;
+  printw("Enter a name: ");
+  refresh();
+  getstr(name);
+  printw("Enter a phone number: ");
+  refresh();
+  getstr(phone_number);
+  printw("Enter an email: ");
+  refresh();
+  getstr(email);
+  printw("Enter a department: ");
+  refresh();
+  getstr(dept);
+  printw("Enter a title if applicable (otherwise input 'None'): ");
+  refresh();
+  getstr(title);
+  printw("Enter an affiliation (e.g.: undergraduate): ");
+  refresh();
+  getstr(aff);
+  printw("\n");
+  refresh();
+
+  str_name = name;
+  str_phone_number = phone_number;
+  str_email = email;
+  str_dept = dept;
+  str_title = title;
+  str_aff = aff;
+
+  insert = "INSERT INTO BANKSYS (NAME, PHONE_NUMBER, EMAIL, DEPT, TITLE, AFF) VALUES ('" + str_name + "', '" + str_phone_number + "', '" + str_email + "', '" + str_dept + "', '" + str_title + "', '" + str_aff + "');";
+  rc = sqlite3_exec(db, insert.c_str(), NULL, 0, &error_msg);
+  if (rc != SQLITE_OK) {
+    printw("Error: Cannot execute SQL Query\n");
+    refresh();
+    exit(-1);
+  }
+}
+
+void update_entry(std::string select, std::string update, sqlite3 *db, char *error_msg, int rc) {
+  int column;
+  std::string column_name;
+
+  char current[100];
+  char replace[100];
+
+  std::string str_current;
+  std::string str_replace;
+  while (true) {
+    erase();
+    printw("\n\n***** Updating student entry *****\n\n");
+    printw("What would you like to update: \n");
+    printw("1. Name\n");
+    printw("2. Phone number\n");
+    printw("3. Email\n");
+    printw("4. Department\n");
+    printw("5. Title\n");
+    printw("6. Affiliation\n");
+    printw("7. Exit\n\n");
+    refresh();
+    column = getch();
+
+    if (column == 49) {
+      column_name = "NAME";
+    } else if (column == 50) {
+      column_name = "PHONE_NUMBER";
+    } else if (column == 51) {
+      column_name = "EMAIL";
+    } else if (column == 52) {
+      column_name = "DEPT";
+    } else if (column == 53) {
+      column_name = "TITLE";
+    } else if (column == 54) {
+      column_name = "AFF";
+    } else {
+      printw("\n");
+      refresh();
+      break;
+    }
+    printw("\n");
+    printw("Enter current value: ");
+    refresh();
+    getstr(current);
+    str_current = current;
+    printw("Enter new value: ");
+    refresh();
+    getstr(replace);
+    str_replace = replace;
+
+    update = "UPDATE BANKSYS SET " + column_name + "='" + str_replace + "' WHERE " + column_name + "='" + str_current + "';";
+    rc = sqlite3_exec(db, update.c_str(), NULL, 0, &error_msg);
+    if (rc != SQLITE_OK) {
+      printw("Error: Cannot execute SQL Query\n");
+      refresh();
+      exit(-1);
+    }
+    printw("\n");
+  }
+}
+
+void delete_entry(std::string select, std::string del, sqlite3 *db, char *error_msg, int rc) {
+  printw("\n***** Delete student entry *****\n\n");
+  refresh();
+  select = "SELECT * FROM BANKSYS;";
+  rc = sqlite3_exec(db, select.c_str(), NULL, 0, &error_msg);
+  if (rc != SQLITE_OK) {
+    printw("Error: Cannot execute SQL Query\n");
+    refresh();
+    exit(-1);
+  }
+  int delete_choice;
+  printw("Delete based on: \n");
+  printw("1. Name\n");
+  printw("2. Phone number\n");
+  printw("3. Email\n");
+  printw("4. Department\n");
+  printw("5. Title\n");
+  printw("6. Affiliation\n");
+  printw("\n");
+  refresh();
+  delete_choice = getch();
+  printw("\n\n");
+  refresh();
+
+  if (delete_choice == 49) {
+    printw("Enter a name: ");
+    refresh();
+    getstr(name);
+    str_name = name;
+    del = "DELETE FROM BANKSYS WHERE NAME='" + str_name + "';";
+  } else if (delete_choice == 50) {
+    printw("Enter a phone number: ");
+    refresh();
+    getstr(phone_number);
+    str_phone_number = phone_number;
+    del = "DELETE FROM BANKSYS WHERE PHONE_NUMBER='" + str_phone_number + "';";
+  } else if (delete_choice == 51) {
+    printw("Enter an email: ");
+    refresh();
+    getstr(email);
+    str_email = email;
+    del = "DELETE FROM BANKSYS WHERE EMAIL='" + str_email + "';";
+  } else if (delete_choice == 52) {
+    printw("Enter a department: ");
+    refresh();
+    getstr(dept);
+    str_dept = dept;
+    del = "DELETE FROM BANKSYS WHERE DEPT='" + str_dept + "';";
+  } else if (delete_choice == 53) {
+    printw("Enter a title: ");
+    refresh();
+    getstr(title);
+    str_title = title;
+    del = "DELETE FROM BANKSYS WHERE TITLE='" + str_title + "';";
+  } else if (delete_choice == 54) {
+    printw("Enter an affiliation: ");
+    refresh();
+    getstr(aff);
+    str_aff = aff;
+    del = "DELETE FROM BANKSYS WHERE AFF='" + str_aff + "';";
+  } else {
+    printw("Error: Choice unavailable\n\n");
+    refresh();
+  }
+  rc = sqlite3_exec(db, del.c_str(), NULL, 0, &error_msg);
+  if (rc != SQLITE_OK) {
+    printw("Error: Cannot execute SQL Query\n");
+    refresh();
+    exit(-1);
+  }
+  printw("\n");
+  refresh();
 }
 
 int main(int argc, char **argv) {
@@ -31,6 +221,8 @@ int main(int argc, char **argv) {
   // Database Variables
   sqlite3 *db;
   int rc;
+  char *error_msg;
+  std::fstream fs;
 
   // Database Actions
   std::string create;
@@ -38,48 +230,11 @@ int main(int argc, char **argv) {
   std::string select;
   std::string del;
   std::string update;
-
-  // Column Names
-  char name[100];
-  setZero(name);
-  char phone_number[100];
-  setZero(phone_number);
-  char email[100];
-  setZero(email);
-  char dept[100];
-  setZero(dept);
-  char title[100];
-  setZero(title);
-  char aff[100];
-  setZero(aff);
-
-  std::string str_name;
-  std::string str_phone_number;
-  std::string str_email;
-  std::string str_dept;
-  std::string str_title;
-  std::string str_aff;
-
-  // UPDATE variables
-  int column;
-  std::string column_name;
-
-  char current[100];
-  setZero(current);
-  char replace[100];
-  setZero(replace);
-
-  std::string str_current;
-  std::string str_replace;
-
-  // MISC variables
-  char *error_msg;
-  std::fstream fs;
   
-  rc = sqlite3_open("copy.db", &db);
+  rc = sqlite3_open("records.db", &db);
 
   // Find out size of database if it exists
-  std::ifstream is ("copy.db", std::ifstream::binary);
+  std::ifstream is ("records.db", std::ifstream::binary);
   is.seekg (0, is.end);
   int length = is.tellg();
   is.seekg (0, is.beg);
@@ -91,7 +246,6 @@ int main(int argc, char **argv) {
       refresh();
       return -1;
     }
-
     // Create a table
     create = "CREATE TABLE BANKSYS( "
           "ID INTEGER PRIMARY KEY, "
@@ -112,6 +266,10 @@ int main(int argc, char **argv) {
   while (true) {
     erase();
     // Main Menu
+    start_color();
+    init_pair(1, COLOR_YELLOW, COLOR_BLACK);
+    attron(A_BOLD);
+    attron(COLOR_PAIR(1));
     printw("\n\n********** Welcome to the Student Records Database **********\n\n\n");
     printw("1. Add student entry\n");
     printw("2. Update student information\n");
@@ -122,185 +280,23 @@ int main(int argc, char **argv) {
     refresh();
     int choice = getch();
     printw("\n");
+    attroff(COLOR_PAIR(1));
+    attroff(A_BOLD);
     refresh();
     erase();
     
     switch (choice) {
     // ADD ENTRY
       case 49:
-        printw("\n\n***** Adding student entry *****\n\n");
-        refresh();
-
-        select = "SELECT * FROM BANKSYS;";
-        rc = sqlite3_exec(db, select.c_str(), NULL, 0, &error_msg);
-        if (rc != SQLITE_OK) {
-          printw("Error: Cannot execute SELECT Query in ADD ENTRY\n");
-          refresh();
-          return -1;
-        }
-        printw("Enter a name: ");
-        refresh();
-        getstr(name);
-        printw("Enter a phone number: ");
-        refresh();
-        getstr(phone_number);
-        printw("Enter an email: ");
-        refresh();
-        getstr(email);
-        printw("Enter a department: ");
-        refresh();
-        getstr(dept);
-        printw("Enter a title if applicable (otherwise input 'None'): ");
-        refresh();
-        getstr(title);
-        printw("Enter an affiliation (e.g.: undergraduate): ");
-        refresh();
-        getstr(aff);
-        printw("\n");
-        refresh();
-
-        str_name = name;
-        str_phone_number = phone_number;
-        str_email = email;
-        str_dept = dept;
-        str_title = title;
-        str_aff = aff;
-
-        insert = "INSERT INTO BANKSYS (NAME, PHONE_NUMBER, EMAIL, DEPT, TITLE, AFF) VALUES ('" + str_name + "', '" + str_phone_number + "', '" + str_email + "', '" + str_dept + "', '" + str_title + "', '" + str_aff + "');";
-        rc = sqlite3_exec(db, insert.c_str(), NULL, 0, &error_msg);
-        if (rc != SQLITE_OK) {
-          printw("Error: Cannot execute SQL Query\n");
-          refresh();
-          return -1;
-        }
+        add_entry(select, insert, db, error_msg, rc);
         break;
     // UPDATE ENTRY
       case 50:
-        while (true) {
-          erase();
-          printw("\n\n***** Updating student entry *****\n\n");
-          printw("What would you like to update: \n");
-          printw("1. Name\n");
-          printw("2. Phone number\n");
-          printw("3. Email\n");
-          printw("4. Department\n");
-          printw("5. Title\n");
-          printw("6. Affiliation\n");
-          printw("7. Exit\n\n");
-          refresh();
-          column = getch();
-
-          if (column == 49) {
-            column_name = "NAME";
-          } else if (column == 50) {
-            column_name = "PHONE_NUMBER";
-          } else if (column == 51) {
-            column_name = "EMAIL";
-          } else if (column == 52) {
-            column_name = "DEPT";
-          } else if (column == 53) {
-            column_name = "TITLE";
-          } else if (column == 54) {
-            column_name = "AFF";
-          } else {
-            printw("\n");
-            refresh();
-            break;
-          }
-          printw("\n");
-          printw("Enter current value: ");
-          refresh();
-          getstr(current);
-          str_current = current;
-          printw("Enter new value: ");
-          refresh();
-          getstr(replace);
-          str_replace = replace;
-
-          update = "UPDATE BANKSYS SET " + column_name + "='" + str_replace + "' WHERE " + column_name + "='" + str_current + "';";
-          rc = sqlite3_exec(db, update.c_str(), NULL, 0, &error_msg);
-          if (rc != SQLITE_OK) {
-            printw("Error: Cannot execute SQL Query\n");
-            refresh();
-            return -1;
-          }
-          printw("\n");
-        }
+        update_entry(select, update, db, error_msg, rc);
         break;
     // DELETE ENTRY
       case 51:
-        printw("\n***** Delete student entry *****\n\n");
-        refresh();
-        select = "SELECT * FROM BANKSYS;";
-        rc = sqlite3_exec(db, select.c_str(), NULL, 0, &error_msg);
-        if (rc != SQLITE_OK) {
-          printw("Error: Cannot execute SQL Query\n");
-          refresh();
-          return -1;
-        }
-        int delete_choice;
-        printw("Delete based on: \n");
-        printw("1. Name\n");
-        printw("2. Phone number\n");
-        printw("3. Email\n");
-        printw("4. Department\n");
-        printw("5. Title\n");
-        printw("6. Affiliation\n");
-        printw("\n");
-        refresh();
-        delete_choice = getch();
-        printw("\n\n");
-        refresh();
-
-        if (delete_choice == 49) {
-          printw("Enter a name: ");
-          refresh();
-          getstr(name);
-          str_name = name;
-          del = "DELETE FROM BANKSYS WHERE NAME='" + str_name + "';";
-        } else if (delete_choice == 50) {
-          printw("Enter a phone number: ");
-          refresh();
-          getstr(phone_number);
-          str_phone_number = phone_number;
-          del = "DELETE FROM BANKSYS WHERE PHONE_NUMBER='" + str_phone_number + "';";
-        } else if (delete_choice == 51) {
-          printw("Enter an email: ");
-          refresh();
-          getstr(email);
-          str_email = email;
-          del = "DELETE FROM BANKSYS WHERE EMAIL='" + str_email + "';";
-        } else if (delete_choice == 52) {
-          printw("Enter a department: ");
-          refresh();
-          getstr(dept);
-          str_dept = dept;
-          del = "DELETE FROM BANKSYS WHERE DEPT='" + str_dept + "';";
-        } else if (delete_choice == 53) {
-          printw("Enter a title: ");
-          refresh();
-          getstr(title);
-          str_title = title;
-          del = "DELETE FROM BANKSYS WHERE TITLE='" + str_title + "';";
-        } else if (delete_choice == 54) {
-          printw("Enter an affiliation: ");
-          refresh();
-          getstr(aff);
-          str_aff = aff;
-          del = "DELETE FROM BANKSYS WHERE AFF='" + str_aff + "';";
-        } else {
-          printw("Error: Choice unavailable\n\n");
-          refresh();
-          continue;
-        }
-        rc = sqlite3_exec(db, del.c_str(), NULL, 0, &error_msg);
-        if (rc != SQLITE_OK) {
-          printw("Error: Cannot execute SQL Query\n");
-          refresh();
-          return -1;
-        }
-        printw("\n");
-        refresh();
+        delete_entry(select, del, db, error_msg, rc);
         break;
     // SHOW TABLE ENTRIES
       case 52:
