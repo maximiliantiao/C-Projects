2396f974b24a42d1bfd173a791f7d58af1376031
Maximilian Tiao <maximilian.tiao@gmail.com>
2020-05-11 21:43:29 -0700

    New Project for the summer possible

---
+++ C++ Projects/Citation Program/citation.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+  std::cout << "Welcome to the Citation Program!" << "\n\n";
+  
+  std::cout << "1. Create a new citation\n";
+  std::cout << "2. Edit an existing citation\n";
+  std::cout << "3. Delete a citation\n";
+  std::cout << "4. View a citation\n";
+  std::cout << "5. Exit\n";
+  
+   // https://owl.purdue.edu/owl/research_and_citation/mla_style/mla_formatting_and_style_guide/mla_formatting_and_style_guide.html
+  return 0;
+}
